d702a36fbf8682833d1c90a24b40c41288631981
dlutwindows <63083676+dlutwindows@users.noreply.github.com>
2020-04-03 10:29:35 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HelloWorld{
+	public static void main(String[] args){
+		int num=2;
+		int sum=0;
+		while(num<=100){
+			sum+=num;
+			num+=2;
+		}
+		System.out.println(sum);
+	}
+}
+
